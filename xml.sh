f674e709c105e342b3f0d2d29d11a2c0d14f7a52
Iulian-Coroban <iulian.coroban62@gmail.com>
2026-01-11 19:43:27 +0200

    #2 xml.sh(fisier principal)
    In acest fisier au loc case-urile iar acestea se ocupa de chemarea fiecarui feature (validate, list, set, get, insert si prettify) in functie de flag-ul pasat in lina de comanda. De asemenea se ocupa si de verificarea validitatii parametrilor transmisi prin linia de comanda(adica daca numarul de parametri este insuficient pentru un anumit feature va afisa "Parametri invalizi")
    De asemenea inainte de toate va verifica validitatea fisierului xml iar daca acesta este invalid nu va apela niciun feature cerut.

---
+++ xml.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+#verificare daca exita fisierul trimis ca paremetru la linia de comanda
+if [ $# -lt 1 ]; then
+    exit 1
+fi
+
+file="$1"
+shift
+
+
+tag=""
+item=""
+valoare=""
+
+out="$(./validate.sh "$file" | tr -d '\r\n')"
+
+if [[ "$out" == "XML Invalid" ]]; then
+    echo "XML Invalid"
+    exit 0
+fi
+
+
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -validate)
+            ./validate.sh "$file"
+            shift
+            ;;  
+        -list)
+            ./list.sh "$file"
+            shift
+            ;;
+        -set)
+            if [ $# -lt 3 ]; then
+                echo "Parametri invalizi"
+                exit 1
+            fi
+
+            item="$2"
+            valoare="$3"
+            ./set.sh "$file" "$item" "$valoare"
+            shift 3
+            ;;
+        -get)
+            if [ $# -lt 2 ]; then
+                echo "Parametri invalizi"
+                exit 1
+            fi
+            tag="$2"
+            ./get.sh "$file" "$tag"
+            shift 2
+            ;;
+       -insert)
+            if [ $# -lt 3 ]; then
+                echo "Parametri invalizi"
+                exit 1
+            fi
+            item="$2"
+            tag="$3"
+            ./insert.sh "$file" "$item" "$tag"
+            shift 3
+           ;;
+        -prettify)
+            ./prettify.sh "$file" 
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+exit 1
+
+
+
